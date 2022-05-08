1d54e16a597a26e548d250b7211324c0f900df42
Neeiser <stortini.study@gmail.com>
2022-05-08 17:13:52 +0200

    some scss style

--- resources/views/TrainView.blade.php
+++ resources/views/TrainView.blade.php
@@ -5,10 +5,52 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>laravel-migration-seeder</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
 <body>
-    @foreach ($trains as $train)
-        <p>{{ $train->nome_azienda}}</p>
-    @endforeach
+    <main>
+        <div class="train-container">
+            @foreach ($trains as $train)
+            <div class="train-box">
+                <div class="train-category">
+                    <strong>Nome Azienda:</strong>
+                    <span>{{ $train->nome_azienda }}</span>
+                </div>
+                <div class="train-category">
+                    <strong>Stazione di Partenza</strong>
+                    <span>{{ $train->stazione_di_partenza }}</span>
+                </div>
+                <div class="train-category">
+                    <strong>Stazione di Arrivo</strong>
+                    <span>{{ $train->stazione_di_arrivo }}</span>
+                </div>
+                <div class="train-category">
+                    <strong>Orario di Partenza</strong>
+                    <span>{{ $train->orario_di_partenza }}</span>
+                </div>
+                <div class="train-category">
+                    <strong>Orario di arrivo</strong>
+                    <span>{{ $train->orario_di_arrivo }}</span>
+                </div>
+                <div class="train-category">
+                    <strong>Codice Treno</strong>
+                    <span>{{ $train->codice_treno }}</span>
+                </div>
+                <div class="train-category">
+                    <strong>Numero di Carrozze</strong>
+                    <span>{{ $train->numero_tarrozze }}</span>
+                </div>
+                <div class="train-category">
+                    <strong>In orario</strong>
+                    <span>{{ $train->in_orario }}</span>
+                </div>
+                <div class="train-category">
+                    <strong>Cancellato</strong>
+                    <span>{{ $train->cancellato }}</span>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </main>
 </body>
 </html>
